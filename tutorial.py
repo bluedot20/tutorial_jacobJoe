ebe0851044a1f943f17de3e27791311840e2ddfc
logncoding <logncoding@logncodingui-MacBookAir.local>
2022-05-15 19:20:28 +0900

    this is my github tutorial sample message

---
+++ tutorial.py
@@ -0,0 +1,11 @@
+def rotateString(s, k):
+# Write the function rotateString(s, k) that takes a string s 
+# If k is non-negative, the function returns the string s rotated 
+# k places to the left. 
+
+# assert(rotateString('abcd',  1) == 'bcda')
+
+	return 
+
+def testRotateString(): 
+	assert(rotateString('abcd',  1) == 'bcda')
